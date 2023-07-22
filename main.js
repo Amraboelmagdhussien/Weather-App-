1897d455d87888f9f5b58bfa847d920adea9c40e
unknown <amraboelmagdhussien@gmail.com>
2023-07-22 19:10:33 +0300

    Basic Card Weather App V1

---
+++ main.js
@@ -0,0 +1,53 @@
+const apiKey = "cfb3485b9fa911a64482b82361f8dd74";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main = "Clouds") {
+        weatherIcon.src = "images/clouds.png"
+        document.body.style.backgroundImage = "./images/727.png";
+
+    } else if (data.weather[0].main = "Clear" ) {
+        weatherIcon.src = "images/clear.png"
+
+    } else if (data.weather[0].main = "Mist") {
+        weatherIcon.src = "images/mist.png"
+
+    } else if (data.weather[0].main = "Drizzle") {
+        weatherIcon.src = "images/drizzle.png"
+
+    } else if (data.weather[0].main = "Rain") {
+        weatherIcon.src = "images/rain.png"
+
+    }
+    else if (data.weather[0].main = "Snow") {
+        weatherIcon.src = "images/snow.png"
+
+    }
+
+document.querySelector(".weather").style.display = "block"
+
+
+
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+})
+
+
